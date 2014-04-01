ba25c3381905cc8a8411ab31bccb674e679f56a4
nguyenvinhlinh <nguyenvinhlinh@zing.vn>
2014-04-01 17:59:56 +0700

    finished download mode

--- init.js
+++ init.js
@@ -10,7 +10,8 @@ define_key(content_buffer_normal_keymap, "C-x f", "follow-new-buffer");
 define_key(default_global_keymap, "C-k", "kill-current-buffer");
 define_webjump("g", "https://www.google.com.vn/?gws_rd=cr&ei=K_QCU4GdNI3skgXGuoHgBQ#q=%s");
 define_webjump("y", "http://www.youtube.com/results?search_query=%s");
-define_key(default_global_keymap, "C-D", "download-youtube-video" );
+define_key(default_global_keymap, "C-M", "download-video-2-music" );
+define_key(default_global_keymap, "C-D", "download-video-2-downloads" );
 //auto save exist buffers in conkeror
 require("session.js"); 
 session_auto_save_auto_load = true; 
@@ -82,10 +83,18 @@ define_key(default_global_keymap, "M-r",
           });
 
 //download video from youtube using cclive
-interactive("download-youtube-video","Download current playing video.",
+interactive("download-video-2-music","Download current playing video.",
 			function(I){
 			  var path = I.buffer.current_uri.path;
 			  path = path.substr(9,30);
-			  //I.minibuffer.message("Link is: "+ newUrl);
-			  shell_command_blind("cclive --output-dir=/home/nguyenvinhlinh/Downloads \"https://www.youtube.com/watch?v=\""+ path);
+   			  I.minibuffer.message("Downloading video to folder Music ");
+			  shell_command_blind("cclive -d /home/nguyenvinhlinh/Music \"https://www.youtube.com/watch?v=\""+ path);
+			});
+
+interactive("download-video-2-downloads","Download current playing video.",
+			function(I){
+			  var path = I.buffer.current_uri.path;
+			  path = path.substr(9,30);
+			  I.minibuffer.message("Downloading video to folder Downloads");
+			  shell_command_blind("cclive -d /home/nguyenvinhlinh/Downloads \"https://www.youtube.com/watch?v=\""+ path);
 			});
