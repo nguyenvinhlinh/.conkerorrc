179fab159af71b69fdb617f84ddab9e3a6150739
nguyenvinhlinh <nguyenvinhlinh93@gmail.com>
2015-06-16 18:38:19 +0700

    add aria2c-web to web shortcut

--- config/linh-url-shortcut.js
+++ config/linh-url-shortcut.js
@@ -2,9 +2,11 @@ interactive("open-hacker-news", "Go to hacker news", "follow-new-buffer", $brows
 interactive("open-jupiter-broadcasting", "Go to jupiter broadcasting", "follow-new-buffer", $browser_object="http://www.jupiterbroadcasting.com/");
 interactive("open-nvl-blog", "Go to jupiter broadcasting", "follow-new-buffer", $browser_object="http://nguyenvinhlinh.github.io");
 interactive("open-github", "Go to github", "follow-new-buffer", $browser_object="https://github.com/");
+interactive("open-aria2c", "Go to aria2c-web", "follow-new-buffer", $browser_object="file:///home/nguyenvinhlinh/Software/webui-aria2/index.html");
 
 define_key(content_buffer_normal_keymap, "f1", "open-hacker-news");
 define_key(content_buffer_normal_keymap, "f2", "open-jupiter-broadcasting");
 define_key(content_buffer_normal_keymap, "f3", "open-github")
 define_key(content_buffer_normal_keymap, "f4", "open-nvl-blog")
+define_key(content_buffer_normal_keymap, "f5", "open-aria2c")
 define_key(content_buffer_normal_keymap, "C-p", "paste-url-new-buffer");
