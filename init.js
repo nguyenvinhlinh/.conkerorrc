e06022432c7f0872d71e16a39c8672aaea4f5678
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2015-07-25 10:46:10 +0700

    alternative ESC conf move to linh-general-keymap

--- init.js
+++ init.js
@@ -11,14 +11,10 @@ nvl_add_path("conkeror-extended-haivl-mode");
 //define default download directory
 cwd=get_home_directory();
 cwd.append("Downloads");
-/// alternative key for ESC
-require("global-overlay-keymap");
-define_key_alias("M-o", "escape");
 // mode line
 require("mode-line.js");
 add_hook("mode_line_hook", mode_line_adder(buffer_count_widget), true);
 add_hook("mode_line_hook", mode_line_adder(loading_count_widget), true);
-
 
 require('page-modes/google-search-results.js');
 clock_time_format = "%R %B %d, %Y"
@@ -35,11 +31,11 @@ require("linh-history-delete.js");
 require("linh-youtube.js");
 require("clicks-in-new-buffer.js");
 require("linh-general-keymap.js");
+
+url_completion_use_bookmarks = true;
+url_completion_use_history = true;
 session_auto_save_auto_load = true; 
 url_completion_use_history = true;
 session_pref('browser.history_expire_days', 365);
 tab_bar_show_icon=true;
 tab_bar_show_index=true;
-//view history 
-url_completion_use_bookmarks = true;
-url_completion_use_history = true;
